d4b06918e45a906f50df348c7ab522cbb71a30a5
bengsky13 <bengskysec13@gmail.com>
2024-07-28 19:18:06 +0700

    setup.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import cups
 import time
 import requests
 
-baseURL = "http://eprint.id/api"
+baseURL = "http://eprints.id/api"
 API_KEY = os.getenv("EPRINT_API_KEY")
 app = Flask(__name__)
 CORS(app)
